f83569cf7ef0ef319754783a8c7c48169483a6e6
Josh Cerri <joshcerrilight@gmail.com>
2017-06-21 01:22:20 -0500

    GUI initial graphic design complete.

--- Enc1.GUI.py
+++ Enc1.GUI.py
@@ -11,13 +11,31 @@ root = Tk()
 
 #Window setup
 root.title("X-Wing Encoder Initialization")
-root.geometry("300x50")
+root.geometry("375x125")
 
 #Create frame/widget holder.
 app = Frame(root)
 app.grid()
-label = Label(app, text="Encoder Configuration Utility")
-label.grid()
+
+#Create Labels
+Label(text="Please enter the following information").grid(row=0, column=1)
+Label(text="Server IP").grid(row=1, column=0)
+Label(text="Port").grid(row=2, column=0)
+Label(text="Baud Rate").grid(row=3, column=0)
+
+e1=Entry(text="10.0.0.1")
+e2=Entry(text="5100")
+e3=Entry(text="9600")
+
+e1.grid(row=1, column =1)
+e2.grid(row=2, column =1)
+e3.grid(row=3, column =1)
+
+#Create Buttons
+
+applybutton = Button(text="Apply").grid(row=4, column=3)
+cancelbutton = Button(text="Cancel").grid(row=4, column=2)
+acceptbutton = Button(text="OK!").grid(row=4, column=0)
 
 #Begin event loop
 root.mainloop()
