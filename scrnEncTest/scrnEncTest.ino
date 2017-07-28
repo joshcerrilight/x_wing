f764c3306d9c043300cdc10813cf447231571744
MessyMilfD <joshcerrilight@gmail.com>
2017-07-28 18:30:13 -0400

    Update scrnEncTest.ino

--- scrnEncTest/scrnEncTest.ino
+++ scrnEncTest/scrnEncTest.ino
@@ -3,50 +3,44 @@
 
 LiquidCrystal lcd(7,6,5,4,3,2);
 
+//Initiate lcd and setup encoder
 void setup(){
 	lcd.begin(16,2);
-	lcd.setCursor(0,0);
 	int encEnt=10;
 	pinMode(encEnt,INPUT_PULLUP);
-        
-
 }
+
+//Set some global variables
 Encoder menuCntrl(8,9);
 const int encEnt = 10;
 long encPos = -999;
-	
+int page = 0;
+int item = 1;
+int outValue = 1;
+
+
+//Loop it up. Here come some lines of code, brah.	
 void loop(){
         long newPos = menuCntrl.read();
         int encPush = digitalRead(encEnt);
-   
-           if(newPos < 100){
-            if(newPos %4 == 0){
-              if(newPos > encPos){
-                lcd.clear();
-	        lcd.print("RIGHT BUTTSTUFF");
-                encPos=newPos;
-            }
-               if(newPos < encPos){
-                 lcd.clear();
-                 lcd.print("LEFT BUTTSTUFF");
-                 encPos=newPos;
-             }               
-           if(newPos>=96){
-              menuCntrl.write(0);
-           }
-           if(newPos<=-96){
-             menuCntrl.write(0);
-           }
-           if(encPush == LOW){
-              lcd.clear();
-              lcd.write("NO BUTT STUFF 4U");              
-              delay(1500);
-             lcd.clear();
-            lcd.print("GOTCHA BITCH!!");
-           } 
-           
-             }
-         
-}
+if(page==0){
+	lcd.setCursor(0,0);	
+	lcd.clear();
+	lcd.write("joshcerrilight");
+	delay(50);	
+	if(encPush==0){
+		page=1;
+	}		
 }
 
+if(page==1){
+	lcd.setCursor(0,0);
+	lcd.clear();
+	lcd.write("Intensity");
+	delay(50);
+	if(encPush==0){
+		page=1.1;
+	}
+}
+
+}
